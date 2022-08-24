cc57792b80d43a542ca8d96abf2e86534dbd967b
Jacky Zhang <jackyyzhang@gmail.com>
2022-08-23 21:15:14 -0400

    refactor: restructure project

--- src/main/java/com/example/paperexchange/exceptions/ApiException.java
+++ src/main/java/com/example/paperexchange/exceptions/ApiException.java
@@ -1,4 +1,4 @@
-package com.example.paperexchange.exception;
+package com.example.paperexchange.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
