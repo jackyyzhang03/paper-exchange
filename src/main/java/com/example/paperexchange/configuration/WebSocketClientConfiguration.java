2439ce959a062e417e0cb3f4f94f62e5f5a1e3ef
Jacky Zhang <jackyyzhang@gmail.com>
2022-08-23 22:47:31 -0400

    chore: remove exposed api key

--- src/main/java/com/example/paperexchange/configuration/WebSocketClientConfiguration.java
+++ src/main/java/com/example/paperexchange/configuration/WebSocketClientConfiguration.java
@@ -2,6 +2,7 @@ package com.example.paperexchange.configuration;
 
 import com.example.paperexchange.websocket.FinnhubWebSocketHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.client.WebSocketClient;
@@ -13,6 +14,9 @@ public class WebSocketClientConfiguration {
     @Autowired
     public FinnhubWebSocketHandler webSocketHandler;
 
+    @Value("${finnhub.token}")
+    private String token;
+
     @Bean
     public WebSocketClient webSocketClient() {
         return new StandardWebSocketClient();
@@ -23,7 +27,7 @@ public class WebSocketClientConfiguration {
         WebSocketConnectionManager manager = new WebSocketConnectionManager(
                 webSocketClient(),
                 webSocketHandler,
-                "wss://ws.finnhub.io?token=***REMOVED***"
+                "wss://ws.finnhub.io?token=" + token
         );
         manager.setAutoStartup(true);
         return manager;
